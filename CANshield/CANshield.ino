e8aa0d0643856ad6be76b65a3b89c924b05fcebd
John Fletcher <john@bunbury28.plus.com>
2023-11-27 21:38:41 +0000

    Create CANshield.ino
    Add release 1b

---
+++ CANshield/CANshield.ino
@@ -0,0 +1,442 @@
+
+/////////////////////////////////////////////////////////////////////////////////
+// CANshield
+// This code is intended for the testing of the Arduino CAN shield (MERG Kit 110).
+// The shield plugs into a UNO or MEGA (or clone) and has a NANO plugged in if assembled for that option.
+// This code should be installed on the Arduino when the shield has been assembled.
+// The user needs to have a computer with a copy of the Arduino IDE installed (either version 1.8 or 2.x will do).
+// There is a need for USB lead to connect to the Arduino. This needs to match the socket on the Arduino.
+// This code can then be compiled and run without a connection to CBUS. 
+// The expected output has been documented. (DETAILS TO BE ADDED)
+// The module can then be connected to CBUS for further testing.
+////////////////////////////////////////////////////////////////////////////////
+// Release 1b
+////////////////////////////////////////////////////////////////////////////////
+
+/*
+  Copyright (C) Duncan Greenwood 2017 (duncan_greenwood@hotmail.com) 
+            (C) Martin Da Costa  2023 (martin_dacosta@ntlworld.com)
+            (C) John Fletcher    2023 (john@bunbury28.plus.com)
+
+  This work is licensed under the:
+      Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+   To view a copy of this license, visit:
+      http://creativecommons.org/licenses/by-nc-sa/4.0/
+   or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+
+   License summary:
+    You are free to:
+      Share, copy and redistribute the material in any medium or format
+      Adapt, remix, transform, and build upon the material
+
+    The licensor cannot revoke these freedoms as long as you follow the license terms.
+
+    Attribution : You must give appropriate credit, provide a link to the license,
+                  and indicate if changes were made. You may do so in any reasonable manner,
+                  but not in any way that suggests the licensor endorses you or your use.
+
+    NonCommercial : You may not use the material for commercial purposes. **(see note below)
+
+    ShareAlike : If you remix, transform, or build upon the material, you must distribute
+                 your contributions under the same license as the original.
+
+    No additional restrictions : You may not apply legal terms or technological measures that
+                                 legally restrict others from doing anything the license permits.
+
+   ** For commercial use, please contact the original copyright holder(s) to agree licensing terms
+
+    This software is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE
+
+*/
+
+/*
+      3rd party libraries needed for compilation: (not for binary-only distributions)
+
+      Streaming   -- C++ stream style output, v5, (http://arduiniana.org/libraries/streaming/)
+      ACAN2515    -- library to support the MCP2515/25625 CAN controller IC
+*/
+
+///////////////////////////////////////////////////////////////////////////////////
+// Pin Use map for UNO and NANO:
+// Digital pin 2          Interupt CAN
+// Digital pin 3 (PWM)    Not Used
+// Digital pin 4          CBUS Green LED
+// Digital pin 5 (PWM)    Not Used
+// Digital pin 6 (PWM)    Not Used
+// Digital pin 7          CBUS Yellow LED
+// Digital pin 8          CBUS Switch
+// Digital pin 9 (PWM)    Not Used
+// Digital pin 10 (SS)    CS    CAN
+// Digital pin 11 (MOSI)  SI    CAN
+// Digital pin 12 (MISO)  SO    CAN
+// Digital pin 13 (SCK)   Sck   CAN
+
+// Digital / Analog pin 0     Not Used
+// Digital / Analog pin 1     Not Used
+// Digital / Analog pin 2     Not Used
+// Digital / Analog pin 3     Not Used
+// Digital / Analog pin 4     Not Used
+// Digital / Analog pin 5     Not Used
+// Digital / Analog pin 5     Not Used
+//////////////////////////////////////////////////////////////////////////
+
+// 3rd party libraries
+#include <Streaming.h>
+
+// CBUS library header files
+#include <CBUS2515.h>               // CAN controller and CBUS class
+#include <CBUSswitch.h>             // pushbutton switch
+#include <CBUSLED.h>                // CBUS LEDs
+#include <CBUSconfig.h>             // module configuration
+#include <CBUSParams.h>             // CBUS parameters
+#include <cbusdefs.h>               // MERG CBUS constants
+
+// constants
+const byte VER_MAJ = 1;             // code major version
+const char VER_MIN = 'b';           // code minor version
+const byte VER_BETA = 0;            // code beta sub-version
+const byte MODULE_ID = 81;          // CBUS module type (TO BE CHANGED WHEN NOTIFIED - CODE REQUESTED)
+
+const byte LED_GRN = 4;             // CBUS green SLiM LED pin
+const byte LED_YLW = 7;             // CBUS yellow FLiM LED pin
+const byte SWITCH0 = 8;             // CBUS push button switch pin
+
+const unsigned long CAN_OSC_FREQ = 16000000UL;  // Oscillator frequency on the CAN Shield
+
+
+//////////////////////////////////////////////////////////////////////////
+
+//CBUS pins
+const byte CAN_INT_PIN = 2;
+const byte CAN_CS_PIN = 10;
+
+// CBUS objects
+CBUSConfig modconfig;               // configuration object
+CBUS2515 CBUS(&modconfig);          // CBUS object
+CBUSLED ledGrn, ledYlw;             // two LED objects
+CBUSSwitch pb_switch;               // switch object
+
+// module name, must be 7 characters, space padded.
+unsigned char mname[7] = { 'S', 'H', 'I', 'E', 'L', 'D', ' ' };
+
+// forward function declarations
+void eventhandler(byte, CANFrame *);
+void processSerialInput(void);
+void printConfig(void);
+
+//
+/// setup CBUS - runs once at power on from setup()
+//
+void setupCBUS() {
+
+  // set config layout parameters
+  modconfig.EE_NVS_START = 10;
+  modconfig.EE_NUM_NVS = 10;
+  modconfig.EE_EVENTS_START = 50;
+  modconfig.EE_MAX_EVENTS = 32;
+  modconfig.EE_NUM_EVS = 1;
+  modconfig.EE_BYTES_PER_EVENT = (modconfig.EE_NUM_EVS + 4);
+
+  // initialise and load configuration
+  modconfig.setEEPROMtype(EEPROM_INTERNAL);
+  modconfig.begin();
+
+  Serial << F("> mode = ") << ((modconfig.FLiM) ? "FLiM" : "SLiM") << F(", CANID = ") << modconfig.CANID;
+  Serial << F(", NN = ") << modconfig.nodeNum << endl;
+
+  // show code version and copyright notice
+  printConfig();
+
+  // set module parameters
+  CBUSParams params(modconfig);
+  params.setVersion(VER_MAJ, VER_MIN, VER_BETA);
+  params.setModuleId(MODULE_ID);
+  params.setFlags(PF_FLiM | PF_COMBI);
+
+  // assign to CBUS
+  CBUS.setParams(params.getParams());
+  CBUS.setName(mname);
+
+  // set CBUS LED pins and assign to CBUS
+  ledGrn.setPin(LED_GRN);
+  ledYlw.setPin(LED_YLW);
+  CBUS.setLEDs(ledGrn, ledYlw);
+
+  // initialise CBUS switch and assign to CBUS
+  pb_switch.setPin(SWITCH0, LOW);
+  pb_switch.run();
+  CBUS.setSwitch(pb_switch);
+
+  // module reset - if switch is depressed at startup and module is in SLiM mode
+  if (pb_switch.isPressed() && !modconfig.FLiM) {
+    Serial << F("> switch was pressed at startup in SLiM mode") << endl;
+    modconfig.resetModule(ledGrn, ledYlw, pb_switch);
+  }
+
+  // register our CBUS event handler, to receive event messages of learned events
+  CBUS.setEventHandler(eventhandler);
+
+  // set CBUS LEDs to indicate the current mode
+  CBUS.indicateMode(modconfig.FLiM);
+
+  // configure and start CAN bus and CBUS message processing
+  CBUS.setNumBuffers(2, 1);      // more buffers = more memory used, fewer = less
+  CBUS.setOscFreq(CAN_OSC_FREQ);   // select the crystal frequency of the CAN module
+  CBUS.setPins(CAN_CS_PIN, CAN_INT_PIN);   // select pins for CAN bus CE and interrupt connections
+
+  if (!CBUS.begin()) {
+    Serial << F("> error starting CBUS") << endl;
+  }
+}
+
+//
+/// setup - runs once at power on
+//
+
+void setup() {
+
+  Serial.begin (115200);
+  Serial << endl << endl << F("> ** CBUS Arduino Shield ** ") << __FILE__ << endl;
+
+  setupCBUS();
+
+  // end of setup
+  Serial << F("> ready") << endl << endl;
+}
+
+//
+/// loop - runs forever
+//
+
+void loop() {
+
+  //
+  /// do CBUS message, switch and LED processing
+  //
+
+  CBUS.process();
+
+  //
+  /// process console commands
+  //
+
+  processSerialInput();
+
+  //
+  /// check CAN message buffers
+  //
+
+  if (CBUS.canp->receiveBufferPeakCount() > CBUS.canp->receiveBufferSize()) {
+    // Serial << F("> receive buffer overflow") << endl;
+  }
+
+  if (CBUS.canp->transmitBufferPeakCount(0) > CBUS.canp->transmitBufferSize(0)) {
+    // Serial << F("> transmit buffer overflow") << endl;
+  }
+
+  //
+  /// check CAN bus state
+  //
+
+  byte s = CBUS.canp->errorFlagRegister();
+
+  if (s != 0) {
+    // Serial << F("> error flag register is non-zero = ") << s << endl;
+  }
+
+  //
+  /// bottom of loop()
+  //
+}
+
+//
+/// user-defined event processing function
+/// called from the CBUS library when a learned event is received
+/// it receives the event table index and the CAN frame
+//
+
+void eventhandler(byte index, CANFrame *msg) {
+
+  // as an example, display the opcode and the first EV of this event
+
+  Serial << F("> event handler: index = ") << index << F(", opcode = 0x") << _HEX(msg->data[0]) << endl;
+  Serial << F("> EV1 = ") << modconfig.getEventEVval(index, 1) << endl;
+  return;
+}
+//
+/// print code version config details and copyright notice
+//
+
+void printConfig(void) {
+
+  // code version
+  Serial << F("> code version = ") << VER_MAJ << VER_MIN << F(" beta ") << VER_BETA << endl;
+  Serial << F("> compiled on ") << __DATE__ << F(" at ") << __TIME__ << F(", compiler ver = ") << __cplusplus << endl;
+
+  // copyright
+  Serial << F("> © Duncan Greenwood (MERG M5767) 2019") << endl;
+  Serial << F("> © Martin Da Costa (MERG M6223) 2023") << endl;
+  Serial << F("> © John Fletcher (MERG M6777) 2023") << endl;
+  return;
+}
+
+//
+/// command interpreter for serial console input
+//
+
+void processSerialInput(void) {
+
+  byte uev = 0;
+  char msgstr[32], dstr[32];
+
+  if (Serial.available()) {
+
+    char c = Serial.read();
+
+    switch (c) {
+
+    case 'n':
+
+      // node config
+      printConfig();
+
+      // node identity
+      Serial << F("> CBUS node configuration") << endl;
+      Serial << F("> mode = ") << (modconfig.FLiM ? "FLiM" : "SLiM") << F(", CANID = ") << modconfig.CANID << F(", node number = ") << modconfig.nodeNum << endl;
+      Serial << endl;
+      break;
+
+    case 'e':
+
+      // EEPROM learned event data table
+      Serial << F("> stored events ") << endl;
+      Serial << F("  max events = ") << modconfig.EE_MAX_EVENTS << F(" EVs per event = ") << modconfig.EE_NUM_EVS << F(" bytes per event = ") << modconfig.EE_BYTES_PER_EVENT << endl;
+
+      for (byte j = 0; j < modconfig.EE_MAX_EVENTS; j++) {
+        if (modconfig.getEvTableEntry(j) != 0) {
+          ++uev;
+        }
+      }
+
+      Serial << F("  stored events = ") << uev << F(", free = ") << (modconfig.EE_MAX_EVENTS - uev) << endl;
+      Serial << F("  using ") << (uev * modconfig.EE_BYTES_PER_EVENT) << F(" of ") << (modconfig.EE_MAX_EVENTS * modconfig.EE_BYTES_PER_EVENT) << F(" bytes") << endl << endl;
+
+      Serial << F("  Ev#  |  NNhi |  NNlo |  ENhi |  ENlo | ");
+
+      for (byte j = 0; j < (modconfig.EE_NUM_EVS); j++) {
+        sprintf(dstr, "EV%03d | ", j + 1);
+        Serial << dstr;
+      }
+
+      Serial << F("Hash |") << endl;
+
+      Serial << F(" --------------------------------------------------------------") << endl;
+
+      // for each event data line
+      for (byte j = 0; j < modconfig.EE_MAX_EVENTS; j++) {
+
+        if (modconfig.getEvTableEntry(j) != 0) {
+          sprintf(dstr, "  %03d  | ", j);
+          Serial << dstr;
+
+          // for each data byte of this event
+          for (byte e = 0; e < (modconfig.EE_NUM_EVS + 4); e++) {
+            sprintf(dstr, " 0x%02hx | ", modconfig.readEEPROM(modconfig.EE_EVENTS_START + (j * modconfig.EE_BYTES_PER_EVENT) + e));
+            Serial << dstr;
+          }
+
+          sprintf(dstr, "%4d |", modconfig.getEvTableEntry(j));
+          Serial << dstr << endl;
+        }
+      }
+
+      Serial << endl;
+
+      break;
+
+    // NVs
+    case 'v':
+
+      // note NVs number from 1, not 0
+      Serial << "> Node variables" << endl;
+      Serial << F("   NV   Val") << endl;
+      Serial << F("  --------------------") << endl;
+
+      for (byte j = 1; j <= modconfig.EE_NUM_NVS; j++) {
+        byte v = modconfig.readNV(j);
+        sprintf(msgstr, " - %02d : %3hd | 0x%02hx", j, v, v);
+        Serial << msgstr << endl;
+      }
+
+      Serial << endl << endl;
+
+      break;
+
+    // CAN bus status
+    case 'c':
+
+      CBUS.printStatus();
+      break;
+
+    case 'h':
+      // event hash table
+      modconfig.printEvHashTable(false);
+      break;
+
+    case 'y':
+      // reset CAN bus and CBUS message processing
+      CBUS.reset();
+      break;
+
+    case '*':
+      // reboot
+      modconfig.reboot();
+      break;
+
+    case 'm':
+      // free memory
+      Serial << F("> free SRAM = ") << modconfig.freeSRAM() << F(" bytes") << endl;
+      break;
+
+    case 'r':
+      // renegotiate
+      CBUS.renegotiate();
+      break;
+
+    case 'z':
+        // Reset module, clear EEPROM
+        static bool ResetRq = false;
+        static unsigned long ResWaitTime;
+        if (!ResetRq) {
+          // start timeout timer
+          Serial << F(">Reset & EEPROM wipe requested. Press 'z' again within 2 secs to confirm") << endl;
+          ResWaitTime = millis();
+          ResetRq = true;
+        } else {
+          // This is a confirmed request
+          // 2 sec timeout
+          if (ResetRq && ((millis() - ResWaitTime) > 2000)) {
+            Serial << F(">timeout expired, reset not performed") << endl;
+            ResetRq = false;
+          } else {
+            //Request confirmed within timeout
+            Serial << F(">RESETTING AND WIPING EEPROM") << endl;
+            modconfig.resetModule();
+            ResetRq = false;
+          }
+        }
+        break;
+
+
+    case '\r':
+    case '\n':
+      Serial << endl;
+      break;
+
+    default:
+      // Serial << F("> unknown command ") << c << endl;
+      break;
+    }
+  }
+}
