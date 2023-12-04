e1b07b96eb0cb62f4afeb9cc40949191d5c4afb1
John Fletcher <john@bunbury28.plus.com>
2023-12-04 17:12:05 +0000

    Update CANshield_LCDBut.ino
    Build the code

--- CANshield_LCDBut/CANshield_LCDBut.ino
+++ CANshield_LCDBut/CANshield_LCDBut.ino
@@ -11,10 +11,11 @@
 ////////////////////////////////////////////////////////////////////////////
 // CBUS library header files
 #include <CBUS2515.h>            // CAN controller and CBUS class
-#include "LEDControl.h"          // CBUS LEDs
+#include <CBUSconfig.h>          // module configuration
+#include <CBUSLED.h>             // CBUS LEDs
 #include <CBUSconfig.h>          // module configuration
 #include <cbusdefs.h>            // MERG CBUS constants
-#include <CBUSParams.h>
+#include <CBUSParams.h>          // CBUS parameters
 
 ////////////DEFINE MODULE/////////////////////////////////////////////////
 /// Use these values for the CBUS outputs
@@ -25,15 +26,98 @@ int prevbutton = -1;
 unsigned char mname[7] = { 'L', 'C', 'D', 'B', 'u', 't', ' ' };
 
 /// constants
-const byte VER_MAJ = 1;         // code major version
+const byte VER_MAJ = 2;         // code major version
 const char VER_MIN = 'a';       // code minor version
 const byte VER_BETA = 1;        // code beta sub-version
-const byte MODULE_ID = 99;      // CBUS module type
+const byte MODULE_ID = 81;      // CBUS module type for CANshield
 
-const unsigned long CAN_OSC_FREQ = 8000000;     // Oscillator frequency on the CAN2515 board
+const byte LED_GRN = 4;             // CBUS green SLiM LED pin
+const byte LED_YLW = 7;             // CBUS yellow FLiM LED pin
+const byte SWITCH0 = 8;             // CBUS push button switch pin
 
-#define NUM_LEDS 1              // How many LEDs are there?
-#define NUM_SWITCHES 1          // How many switchs are there?
+const unsigned long CAN_OSC_FREQ = 16000000UL;     // Oscillator frequency on the CAN2515 board
+
+//////////////////////////////////////////////////////////////////////////
+
+//CBUS pins
+const byte CAN_INT_PIN = 2;
+const byte CAN_CS_PIN = 15; // Changed from 10 because of the LCD shield.
+
+// CBUS objects
+CBUSConfig modconfig;               // configuration object
+CBUS2515 CBUS(&modconfig);          // CBUS object
+CBUSLED ledGrn, ledYlw;             // two LED objects
+CBUSSwitch pb_switch;               // switch object
+
+// forward function declarations
+void eventhandler(byte, CANFrame *);
+void processSerialInput(void);
+void printConfig(void);
+
+//
+/// setup CBUS - runs once at power on from setup()
+//
+void setupCBUS() {
+
+  // set config layout parameters
+  modconfig.EE_NVS_START = 10;
+  modconfig.EE_NUM_NVS = 10;
+  modconfig.EE_EVENTS_START = 50;
+  modconfig.EE_MAX_EVENTS = 32;
+  modconfig.EE_NUM_EVS = 1;
+  modconfig.EE_BYTES_PER_EVENT = (modconfig.EE_NUM_EVS + 4);
+
+  // initialise and load configuration
+  modconfig.setEEPROMtype(EEPROM_INTERNAL);
+  modconfig.begin();
+
+  Serial << F("> mode = ") << ((modconfig.FLiM) ? "FLiM" : "SLiM") << F(", CANID = ") << modconfig.CANID;
+  Serial << F(", NN = ") << modconfig.nodeNum << endl;
+
+  // show code version and copyright notice
+  printConfig();
+
+  // set module parameters
+  CBUSParams params(modconfig);
+  params.setVersion(VER_MAJ, VER_MIN, VER_BETA);
+  params.setModuleId(MODULE_ID);
+  params.setFlags(PF_FLiM | PF_COMBI);
+
+  // assign to CBUS
+  CBUS.setParams(params.getParams());
+  CBUS.setName(mname);
+
+  // set CBUS LED pins and assign to CBUS
+  ledGrn.setPin(LED_GRN);
+  ledYlw.setPin(LED_YLW);
+  CBUS.setLEDs(ledGrn, ledYlw);
+
+  // initialise CBUS switch and assign to CBUS
+  pb_switch.setPin(SWITCH0, LOW);
+  pb_switch.run();
+  CBUS.setSwitch(pb_switch);
+
+  // module reset - if switch is depressed at startup and module is in SLiM mode
+  if (pb_switch.isPressed() && !modconfig.FLiM) {
+    Serial << F("> switch was pressed at startup in SLiM mode") << endl;
+    modconfig.resetModule(ledGrn, ledYlw, pb_switch);
+  }
+
+  // register our CBUS event handler, to receive event messages of learned events
+  CBUS.setEventHandler(eventhandler);
+
+  // set CBUS LEDs to indicate the current mode
+  CBUS.indicateMode(modconfig.FLiM);
+
+  // configure and start CAN bus and CBUS message processing
+  CBUS.setNumBuffers(2, 1);      // more buffers = more memory used, fewer = less
+  CBUS.setOscFreq(CAN_OSC_FREQ);   // select the crystal frequency of the CAN module
+  CBUS.setPins(CAN_CS_PIN, CAN_INT_PIN);   // select pins for CAN bus CE and interrupt connections
+
+  if (!CBUS.begin()) {
+    Serial << F("> error starting CBUS") << endl;
+  }
+}
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -73,17 +157,52 @@ void setup() {
 
   // Initialise
   Serial.begin(SERIAL_SPEED);             // Start Serial IO.
-  Serial.println("CBUS LCDBut");
+  Serial << endl << endl << F("> ** CBUS LCD Button Arduino Shield ** ") << __FILE__ << endl;
   //analogWrite(pin_d6,50);
  lcd.begin(16, 2);
  lcd.setCursor(0,0);
  lcd.print("CBUS LCDBut");
  lcd.setCursor(0,1);
  lcd.print("Press Key:");
+
+  setupCBUS();
+ // end of setup
+  Serial << F("> ready") << endl << endl;
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+//
+  /// process console commands
+  //
+
+  processSerialInput();
+
+  //
+  /// check CAN message buffers
+  //
+
+  if (CBUS.canp->receiveBufferPeakCount() > CBUS.canp->receiveBufferSize()) {
+    // Serial << F("> receive buffer overflow") << endl;
+  }
+
+  if (CBUS.canp->transmitBufferPeakCount(0) > CBUS.canp->transmitBufferSize(0)) {
+    // Serial << F("> transmit buffer overflow") << endl;
+  }
+
+  //
+  /// check CAN bus state
+  //
+
+  byte s = CBUS.canp->errorFlagRegister();
+
+  if (s != 0) {
+    // Serial << F("> error flag register is non-zero = ") << s << endl;
+  }
+
+
  x = analogRead (0);
  if (x < 175){         // was 50
   range = 1;
@@ -142,3 +261,196 @@ void loop() {
  }
 
 }
+
+//
+/// user-defined event processing function
+/// called from the CBUS library when a learned event is received
+/// it receives the event table index and the CAN frame
+//
+
+void eventhandler(byte index, CANFrame *msg) {
+
+  // as an example, display the opcode and the first EV of this event
+
+  Serial << F("> event handler: index = ") << index << F(", opcode = 0x") << _HEX(msg->data[0]) << endl;
+  Serial << F("> EV1 = ") << modconfig.getEventEVval(index, 1) << endl;
+  return;
+}
+//
+/// print code version config details and copyright notice
+//
+
+void printConfig(void) {
+
+  // code version
+  Serial << F("> code version = ") << VER_MAJ << VER_MIN << F(" beta ") << VER_BETA << endl;
+  Serial << F("> compiled on ") << __DATE__ << F(" at ") << __TIME__ << F(", compiler ver = ") << __cplusplus << endl;
+
+  // copyright
+  Serial << F("> © Duncan Greenwood (MERG M5767) 2019") << endl;
+  Serial << F("> © Martin Da Costa (MERG M6223) 2023") << endl;
+  Serial << F("> © John Fletcher (MERG M6777) 2023") << endl;
+  return;
+}
+
+//
+/// command interpreter for serial console input
+//
+
+void processSerialInput(void) {
+
+  byte uev = 0;
+  char msgstr[32], dstr[32];
+
+  if (Serial.available()) {
+
+    char c = Serial.read();
+
+    switch (c) {
+
+    case 'n':
+
+      // node config
+      printConfig();
+
+      // node identity
+      Serial << F("> CBUS node configuration") << endl;
+      Serial << F("> mode = ") << (modconfig.FLiM ? "FLiM" : "SLiM") << F(", CANID = ") << modconfig.CANID << F(", node number = ") << modconfig.nodeNum << endl;
+      Serial << endl;
+      break;
+
+    case 'e':
+
+      // EEPROM learned event data table
+      Serial << F("> stored events ") << endl;
+      Serial << F("  max events = ") << modconfig.EE_MAX_EVENTS << F(" EVs per event = ") << modconfig.EE_NUM_EVS << F(" bytes per event = ") << modconfig.EE_BYTES_PER_EVENT << endl;
+
+      for (byte j = 0; j < modconfig.EE_MAX_EVENTS; j++) {
+        if (modconfig.getEvTableEntry(j) != 0) {
+          ++uev;
+        }
+      }
+
+      Serial << F("  stored events = ") << uev << F(", free = ") << (modconfig.EE_MAX_EVENTS - uev) << endl;
+      Serial << F("  using ") << (uev * modconfig.EE_BYTES_PER_EVENT) << F(" of ") << (modconfig.EE_MAX_EVENTS * modconfig.EE_BYTES_PER_EVENT) << F(" bytes") << endl << endl;
+
+      Serial << F("  Ev#  |  NNhi |  NNlo |  ENhi |  ENlo | ");
+
+      for (byte j = 0; j < (modconfig.EE_NUM_EVS); j++) {
+        sprintf(dstr, "EV%03d | ", j + 1);
+        Serial << dstr;
+      }
+
+      Serial << F("Hash |") << endl;
+
+      Serial << F(" --------------------------------------------------------------") << endl;
+
+      // for each event data line
+      for (byte j = 0; j < modconfig.EE_MAX_EVENTS; j++) {
+
+        if (modconfig.getEvTableEntry(j) != 0) {
+          sprintf(dstr, "  %03d  | ", j);
+          Serial << dstr;
+
+          // for each data byte of this event
+          for (byte e = 0; e < (modconfig.EE_NUM_EVS + 4); e++) {
+            sprintf(dstr, " 0x%02hx | ", modconfig.readEEPROM(modconfig.EE_EVENTS_START + (j * modconfig.EE_BYTES_PER_EVENT) + e));
+            Serial << dstr;
+          }
+
+          sprintf(dstr, "%4d |", modconfig.getEvTableEntry(j));
+          Serial << dstr << endl;
+        }
+      }
+
+      Serial << endl;
+
+      break;
+
+    // NVs
+    case 'v':
+
+      // note NVs number from 1, not 0
+      Serial << "> Node variables" << endl;
+      Serial << F("   NV   Val") << endl;
+      Serial << F("  --------------------") << endl;
+
+      for (byte j = 1; j <= modconfig.EE_NUM_NVS; j++) {
+        byte v = modconfig.readNV(j);
+        sprintf(msgstr, " - %02d : %3hd | 0x%02hx", j, v, v);
+        Serial << msgstr << endl;
+      }
+
+      Serial << endl << endl;
+
+      break;
+
+    // CAN bus status
+    case 'c':
+
+      CBUS.printStatus();
+      break;
+
+    case 'h':
+      // event hash table
+      modconfig.printEvHashTable(false);
+      break;
+
+    case 'y':
+      // reset CAN bus and CBUS message processing
+      CBUS.reset();
+      break;
+
+    case '*':
+      // reboot
+      modconfig.reboot();
+      break;
+
+    case 'm':
+      // free memory
+      Serial << F("> free SRAM = ") << modconfig.freeSRAM() << F(" bytes") << endl;
+      break;
+
+    case 'r':
+      // renegotiate
+      CBUS.renegotiate();
+      break;
+
+    case 'z':
+        // Reset module, clear EEPROM
+        static bool ResetRq = false;
+        static unsigned long ResWaitTime;
+        if (!ResetRq) {
+          // start timeout timer
+          Serial << F(">Reset & EEPROM wipe requested. Press 'z' again within 2 secs to confirm") << endl;
+          ResWaitTime = millis();
+          ResetRq = true;
+        } else {
+          // This is a confirmed request
+          // 2 sec timeout
+          if (ResetRq && ((millis() - ResWaitTime) > 2000)) {
+            Serial << F(">timeout expired, reset not performed") << endl;
+            ResetRq = false;
+          } else {
+            //Request confirmed within timeout
+            Serial << F(">RESETTING AND WIPING EEPROM") << endl;
+            modconfig.resetModule();
+            ResetRq = false;
+          }
+        }
+        break;
+
+
+    case '\r':
+    case '\n':
+      Serial << endl;
+      break;
+
+    default:
+      // Serial << F("> unknown command ") << c << endl;
+      break;
+    }
+  }
+}
+
+
